dfcfcf0366d045ca97112220345de92bb1d7c5d1
LoganJones <logan.b.jones16@gmail.com>
2020-12-20 00:36:21 -0800

    Added SharedAssets from ML-Agents repo

---
+++ Assets/SharedAssets/Scripts/Utilities.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utilities
+{
+    public static Color32 ToColor(int hexVal)
+    {
+        var r = (byte)((hexVal >> 16) & 0xFF);
+        var g = (byte)((hexVal >> 8) & 0xFF);
+        var b = (byte)(hexVal & 0xFF);
+        return new Color32(r, g, b, 255);
+    }
+}
